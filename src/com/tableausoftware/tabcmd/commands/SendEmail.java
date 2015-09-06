9ed4e7b397db21a32acbf940ab10332916bb7fab
Tamas Foldi <tamas.foldi@ge.com>
2015-09-06 08:54:08 +0200

    Indicate optional arguments

--- src/com/tableausoftware/tabcmd/commands/SendEmail.java
+++ src/com/tableausoftware/tabcmd/commands/SendEmail.java
@@ -67,56 +67,65 @@ public class SendEmail extends Command {
         OptionBuilder.withLongOpt("smtp-host");
         OptionBuilder.withDescription("SMTP Server's address");
         OptionBuilder.hasArg();
+        OptionBuilder.isRequired(true);
         OptionBuilder.withArgName("SMTPSERVER");
-        options.addOption(OptionBuilder.create(""));
+        options.addOption(OptionBuilder.create());
 
         OptionBuilder.withLongOpt("to");
         OptionBuilder.withDescription("Recipient addresses, separated by semicolon (;) character");
         OptionBuilder.hasArg();
+        OptionBuilder.isRequired(true);
         OptionBuilder.withArgName("TO_EMAIL");
-        options.addOption(OptionBuilder.create(""));
+        options.addOption(OptionBuilder.create());
 
         OptionBuilder.withLongOpt("from");
         OptionBuilder.withDescription("From address");
         OptionBuilder.hasArg();
+        OptionBuilder.isRequired(true);
         OptionBuilder.withArgName("FROM_EMAIL");
-        options.addOption(OptionBuilder.create(""));
+        options.addOption(OptionBuilder.create());
 
         OptionBuilder.withLongOpt("subject");
         OptionBuilder.withDescription("Subject of the email");
         OptionBuilder.hasArg();
+        OptionBuilder.isRequired(true);
         OptionBuilder.withArgName("TO_EMAIL");
-        options.addOption(OptionBuilder.create(""));
+        options.addOption(OptionBuilder.create());
 
         OptionBuilder.withLongOpt("file");
         OptionBuilder.withDescription("File to include");
         OptionBuilder.hasArg();
+        OptionBuilder.isRequired(true);
         OptionBuilder.withArgName("FILENAME");
-        options.addOption(OptionBuilder.create(""));
+        options.addOption(OptionBuilder.create());
 
         OptionBuilder.withLongOpt("body");
         OptionBuilder.withDescription("Body Text");
         OptionBuilder.hasArg();
+        OptionBuilder.isRequired(true);
         OptionBuilder.withArgName("EMAIL_TEXT_BODY");
-        options.addOption(OptionBuilder.create(""));
+        options.addOption(OptionBuilder.create());
 
         OptionBuilder.withLongOpt("smtp-port");
         OptionBuilder.withDescription("SMTP Server's port for incoming communication");
         OptionBuilder.hasArg();
+        OptionBuilder.isRequired(false);
         OptionBuilder.withArgName("PORT");
-        options.addOption(OptionBuilder.create(""));
+        options.addOption(OptionBuilder.create());
 
         OptionBuilder.withLongOpt("smtp-user");
         OptionBuilder.withDescription("User name for SMTP authentication");
         OptionBuilder.hasArg();
+        OptionBuilder.isRequired(false);
         OptionBuilder.withArgName("USER");
-        options.addOption(OptionBuilder.create(""));
+        options.addOption(OptionBuilder.create());
 
         OptionBuilder.withLongOpt("smtp-passw");
         OptionBuilder.withDescription("SMTP User's password");
         OptionBuilder.hasArg();
+        OptionBuilder.isRequired(false);
         OptionBuilder.withArgName("PASSWORD");
-        options.addOption(OptionBuilder.create(""));
+        options.addOption(OptionBuilder.create());
 
         return options;
     }
